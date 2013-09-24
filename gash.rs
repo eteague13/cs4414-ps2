b44b6e9ff6d86f16e607e2ce63afa3133134731f
UVa student <student@student-VirtualBox.(none)>
2013-09-23 21:29:48 -0400

    Added support for standard output both append and truncate

--- gash.rs
+++ gash.rs
@@ -1,4 +1,6 @@
 use std::{io, run};
+use std::str;
+use std::path;
 
 
 
@@ -62,21 +64,63 @@ fn main() {
 					}
 					command_history.push_str("\n");
 					
+
 					//Background process
 					if(argv.contains(&~"&")){
 						//Remove & from args so the args are correct
-						let lastIndex = argv.len();
-						argv.remove(lastIndex-1);
+						let lastIndex = argv.len()-1;
+						argv.remove(lastIndex);
 						//transfer ownership
 						let argv = argv;
 						//launch background process with a new schedule
 						do std::task::spawn_sched(std::task::SingleThreaded){
+							//Todo: add clasues for pipes and shit
 							std::run::process_status(program, argv);
 						}
 					}
 					//Foreground process
 					else{
-						run::process_status(program, argv);
+						//check for pipes or io redirect, assuming we have only one
+						if(argv.contains(&~">>") || argv.contains(&~">") || argv.contains(&~"<") || argv.contains(&~"|") ){
+							//std output
+							if(argv.contains(&~">") || argv.contains(&~">>")){
+								//set flags for append or truncate	
+								let mut flags;
+								if(argv.contains(&~">>")){
+									flags = ~[io::Create, io::Append];
+								}
+								else{
+									flags = ~[io::Create, io::Truncate];
+								}
+								//remove args
+								let mut lastIndex = argv.len()-1;
+								let destinationName = &argv.remove(lastIndex); //remove file name
+								argv.remove(lastIndex-1); //remove >
+
+								//store output
+								let mut process_result = run::process_output(program, argv);
+								let mut results = str::from_bytes(process_result.output);
+								let mut outputFile = Path(*destinationName);
+								match io::file_writer(&outputFile, flags){
+									Ok(writer) => {
+										writer.write_line(fmt!("%s", results)); 
+									}
+									Err(err) => {
+										fail!(err)
+									}
+								}
+							}
+							//std input
+							else if(argv.contains(&~"<")){
+
+							}
+							else if(argv.contains(&~"|")){
+
+							}
+						}							
+						else{
+							std::run::process_status(program, argv);
+						}
 					}
 
 				}
